18056eddc0483f49d77f0971a2f36bea96252fd6
Kapouett <kapouett@orange.fr>
2018-10-19 13:20:09 +0200

    Creates a 640*480 window centered on the screen

---
+++ src/arenashooter/Main.java
@@ -0,0 +1,20 @@
+package arenashooter;
+
+import arenashooter.engine.graphics.Window;
+
+public class Main {
+	static Window window;
+	
+	public static void main(String[] args) {
+		window = new Window(640, 480, "Super Blep");
+		
+		while( !window.requestClose() ) {
+			//TODO: mettre le jeu ici
+			
+			window.update();
+		}
+		
+		window.destroy();
+	}
+
+}
