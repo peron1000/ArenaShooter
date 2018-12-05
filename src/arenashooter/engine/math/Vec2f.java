86f374f06efe3089935001a2786d1ec91e19b8d3
Kapouett <kapouett@orange.fr>
2018-12-05 18:30:18 +0100

    Vector multiplication

--- src/arenashooter/engine/math/Vec2f.java
+++ src/arenashooter/engine/math/Vec2f.java
@@ -110,4 +110,14 @@ public class Vec2f {
 	public static Vec2f multiply( Vec2f v, float a ) {
 		return new Vec2f( v.x*a, v.y*a );
 	}
+	
+	/**
+	 * Multiplies two vectors
+	 * @param a
+	 * @param b
+	 * @return ( a.x*b.x, a.y*b.y )
+	 */
+	public static Vec2f multiply( Vec2f a, Vec2f b ) {
+		return new Vec2f( a.x*b.x, a.y*b.y );
+	}
 }
