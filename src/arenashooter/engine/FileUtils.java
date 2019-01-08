2fe31d803f57cb8f21de188a78365c54d34e2117
Kapouett <kapouett@orange.fr>
2019-01-08 22:17:36 +0100

    Moved data out of source

--- src/arenashooter/engine/FileUtils.java
+++ src/arenashooter/engine/FileUtils.java
@@ -2,7 +2,8 @@ package arenashooter.engine;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
@@ -14,44 +15,33 @@ public final class FileUtils {
 	private FileUtils() {}
 	
 	/**
-	 * Read a resource file as a string
-	 * @param path resource path
-	 * @return content of the resource or "" if an error occured
+	 * Read a file as a string
+	 * @param path
+	 * @return content of the file or "" if an error occurred
 	 */
 	public static String resToString(String path) {
 		String res = "";
 		
-		InputStream in = ClassLoader.getSystemResourceAsStream(path);
-		
-		if( in != null) {
+		try {
+			InputStream in = new FileInputStream(new File(path));
 			InputStreamReader inReader = new InputStreamReader(in);
 			BufferedReader reader = new BufferedReader(inReader);
 			
 			String line = "";
-			try {
-				while( (line = reader.readLine()) != null ) {
+			while( (line = reader.readLine()) != null )
 					res += line+"\n"; //Include line breaks
-//					res += line; //Ignore line breaks
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 			
-			try {
-				reader.close();
-				inReader.close();
-				in.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} else {
-			System.err.println("Can't find resource: "+path);
+			reader.close();
+			inReader.close();
+			in.close();
+		} catch(Exception e) {
+			System.err.println("Can't read file: "+path);
 		}
 		
 		return res;
 	}
 	
-	public static ByteBuffer resToByteBuffer(String path) { //TODO: Test this
+	public static ByteBuffer resToByteBuffer(String path) {
 		ByteBuffer res = null;
 		
 		InputStream in;
@@ -59,12 +49,8 @@ public final class FileUtils {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		
 		try {
-			in = ClassLoader.getSystemResourceAsStream(path);
+			in = new FileInputStream(new File(path));
 
-			if( in == null ) {
-				throw new IOException();
-			}
-			
 			int val = in.read();
 			while( val != -1 ) {
 				out.write(val);
