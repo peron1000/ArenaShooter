9a74e219d255add7eb25eb84ff37dfee962260db
Kapouett <kapouett@orange.fr>
2019-01-13 11:31:33 +0100

    Middle-mouse drag in scene view

--- editor/src/application/Affichage.java
+++ editor/src/application/Affichage.java
@@ -5,6 +5,7 @@ import gamedata.entities.Entity;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
@@ -104,6 +105,8 @@ public final class Affichage {
 			selectedName.setText(selected.name);
 	}
 
+	private static double scrollInitialH, scrollInitialV;
+	private static Point2D dragAnchor;
 	private static void createCenterView() {
 		ListEntite.view.setBackground(new Background(new BackgroundFill(Color.rgb(56, 56, 56), new CornerRadii(0), new Insets(0))));
 		ScrollPane scrollContainer = new ScrollPane();
@@ -112,6 +115,32 @@ public final class Affichage {
 		scrollContainer.setVbarPolicy(ScrollBarPolicy.ALWAYS);
 		ListEntite.view.setPadding(new Insets(300));
 		root.setCenter(scrollContainer);
+		
+		//Middle-mouse drag
+		ListEntite.view.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent me) {
+				if(!me.isMiddleButtonDown()) return; //Not middle click
+				
+				//Pour garder le point de depart en memoire
+				scrollInitialH = scrollContainer.getHvalue();
+				scrollInitialV = scrollContainer.getVvalue();
+				dragAnchor = new Point2D(me.getSceneX(), me.getSceneY());
+			}
+		});
+		ListEntite.view.setOnMouseDragged(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent me) {//TODO: Fix movement scaling
+				if(!me.isMiddleButtonDown()) return; //Not middle click
+				
+				//Calcule position apres le drag
+				double dragX = me.getSceneX() - dragAnchor.getX();
+				double dragY = me.getSceneY() - dragAnchor.getY();
+				scrollContainer.setHvalue( scrollInitialH - 1.75*dragX/ListEntite.view.getWidth() );
+				scrollContainer.setVvalue( scrollInitialV - 1.75*dragY/ListEntite.view.getHeight() );
+			}
+		});
 		
 		//Context Menu
 		ContextMenu contextMenu = new ContextMenu();
