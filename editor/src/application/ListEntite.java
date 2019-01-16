7b311e68025d36d50b45c15a853e347d290d7d4b
Kapouett <kapouett@orange.fr>
2019-01-16 17:11:01 +0100

    "Add spatial" buttons

--- editor/src/application/ListEntite.java
+++ editor/src/application/ListEntite.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import application.movableshapes.MovableRectangle;
 import gamedata.entities.Entity;
 import gamedata.entities.Platform;
+import gamedata.entities.Spatial;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -49,8 +50,30 @@ public class ListEntite {
 		view.getChildren().add(visual);
 	}
 	
-	public static void newPlatform() {
-		newPlatform(new Vec2());
+	/**
+	 * Create a spatial and its movable rectangle in the scene view
+	 */
+	public static void newSpatial(Vec2 loc) {
+		Vec2 extent = new Vec2(10, 10);
+		
+		//Snap position to grid
+		int gridSnap = Affichage.gridSnap.getValue();
+		if(gridSnap > 0) {
+			loc.x = extent.x+( Math.floor((loc.x-extent.x)/gridSnap)*gridSnap );
+			loc.y = extent.y+( Math.floor((loc.y-extent.y)/gridSnap)*gridSnap );
+		}
+		
+		Spatial entity = new Spatial(loc, 0);
+		entity.name = "Spatial_"+String.valueOf(System.currentTimeMillis());
+		entity.createProperties();
+		Main.map.children.put(entity.name, entity);
+		Affichage.sceneTree.addEntity(entity);
+		Affichage.selectEntity(entity);
+		
+		//Create movable rectangle attached to the platform
+		Node visual = new MovableRectangle(entity, new Vec2(extent.x*2, extent.y*2), Color.RED);
+		visuals.put(entity, visual);
+		view.getChildren().add(visual);
 	}
 	
 	/**
