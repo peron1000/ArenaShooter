7948ff4dc0ddf89d94042516a55a0309d9126dbf
Kapouett <kapouett@orange.fr>
2019-01-20 18:17:25 +0100

    Fixed loading

--- project/src/arenashooter/game/gameStates/Loading.java
+++ project/src/arenashooter/game/gameStates/Loading.java
@@ -25,19 +25,21 @@ public class Loading extends GameState {
 	private Loading() { }
 	
 	public void init() {
-		Window.camera = new Camera(new Vec3f(0, 0, 450));
 		
 		Window.postProcess = new PostProcess("data/shaders/post_process/pp_loading");
 		
 		ArrayList<Entity> entities = new ArrayList<>();
 		
-		float startX = -(GameMaster.gm.controllers.size()*64f)/2f;
+		Window.camera = new Camera(new Vec3f(0, 0, 600));
+		entities.add(Window.camera);
+		
+		float startX = -(GameMaster.gm.controllers.size()*65f)/2f;
 		int i=0;
 		for(CharacterInfo c : GameMaster.gm.controllers.values()) {
-			float x = startX+(64f*i);
+			float x = startX+(65f*i);
 			
-			entities.add( new CharacterSprite(new Vec2f(x, 0), c) );
-			entities.add( new LoadingFloor(new Vec2f(x, 0)) );
+			entities.add( new CharacterSprite(new Vec2f(x, -30), c) );
+			entities.add( new LoadingFloor(new Vec2f(x, 80)) );
 			i++;
 		}
 		
