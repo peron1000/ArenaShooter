14121d02e8bc44cdc34f0367178b51e3dab8c30e
pmotr <pmotr@DESKTOP-PNKGBHP>
2019-01-22 12:41:23 +0100

    classe Weapon

---
+++ project/src/arenashooter/entities/spatials/items/Weapon.java
@@ -0,0 +1,17 @@
+package arenashooter.entities.spatials.items;
+
+import arenashooter.engine.math.Vec2f;
+
+public class Weapon extends Item {
+
+	public Weapon(Vec2f position, SpritePath itemSprite) {
+		super(position, itemSprite);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void attack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
