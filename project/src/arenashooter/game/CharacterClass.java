98a7c60e3fda9821885f423e00f2b6f583239b96
Kapouett <kapouett@orange.fr>
2019-02-03 16:08:40 +0100

    More foxes

--- project/src/arenashooter/game/CharacterClass.java
+++ project/src/arenashooter/game/CharacterClass.java
@@ -3,7 +3,7 @@ package arenashooter.game;
 public class CharacterClass {
 	public static final CharacterClass 
 		bird = new CharacterClass( "Bird", new String[] {"moineau_01", "canard_01", "canard_02"} ),
-		agile = new CharacterClass( "Agile", new String[] {"chat_01", "renard_01", "renard_02"} ),
+		agile = new CharacterClass( "Agile", new String[] {"chat_01", "renard_01", "renard_02", "renard_03", "renard_04"} ),
 		heavy = new CharacterClass( "Heavy", new String[] {"chevre_01", "vache_01"} );
 	
 	
