ee29052aa5ad348bf521b4eaf85ca877e2fb80db
SnPop <shervinpedaran@yahoo.fr>
2019-03-01 10:39:19 +0100

    map chooser clean

--- project/src/arenashooter/game/gameStates/MapChooser.java
+++ project/src/arenashooter/game/gameStates/MapChooser.java
@@ -1,8 +1,5 @@
 package arenashooter.game.gameStates;
 
-import java.util.Collection;
-import java.util.HashMap;
-import org.lwjgl.system.linux.X11;
 import arenashooter.engine.graphics.PostProcess;
 import arenashooter.engine.graphics.Window;
 import arenashooter.engine.graphics.fonts.Text;
@@ -11,8 +8,7 @@ import arenashooter.engine.input.Device;
 import arenashooter.engine.input.Input;
 import arenashooter.engine.math.Vec2f;
 import arenashooter.engine.math.Vec3f;
-import arenashooter.entities.Controller;
-import arenashooter.entities.spatials.CharacterSprite;
+
 import arenashooter.entities.spatials.Sprite;
 import arenashooter.entities.spatials.TextSpatial;
 import arenashooter.game.GameMaster;
@@ -30,14 +26,6 @@ public class MapChooser extends GameState {
 		return "data/mapXML/" + mapChosen + ".xml";
 	}
 
-	private HashMap<Device, Controller> controllers = new HashMap<>(1);
-	private HashMap<Controller, CharacterSprite> sprites = new HashMap<>(1);
-
-	private int i = -300;
-
-	public Collection<Controller> getControllers() {
-		return controllers.values();
-	}
 
 	@Override
 	public void init() {
@@ -47,48 +35,33 @@ public class MapChooser extends GameState {
 		TextSpatial textEnt = new TextSpatial(new Vec3f(0, -500, -10), new Vec3f(450), text);
 		textEnt.attachToParent(map, "Text_Select");
 
-		Controller controllerKeyboard = new Controller(Device.KEYBOARD);
-		controllers.put(Device.KEYBOARD, controllerKeyboard);
-		i += 150;
 	}
 
 	@Override
 	public void update(double delta) {
-		for (Device device : Device.values()) {
-			if (Input.actionPressed(device, Action.UI_OK) && !controllers.keySet().contains(device)) {
-
+		// Temp sprite changing
+		if (Input.actionJustPressed(Device.KEYBOARD, Action.UI_UP)) {
+			if (init < max - 1) {
+				init++;
 			}
-			// TODO : remove controller when UI_BACK is pressed
-		}
-
-		// Update controllers
-		for (Controller controller : controllers.values()) {
-			controller.step(delta);
-
-			// Temp sprite changing
-			if (Input.actionJustPressed(controller.getDevice(), Action.UI_UP)) {
-				if (init < max - 1) {
-					init++;
-				}
-				else{
-					init=0;
-				}
-				mapChosen = cho[init];
-				getMapChoosen();
-				//String n = ;
-				Sprite sp = new Sprite(new Vec2f(0, 0), "data/MAP_VIS/TREE.png");
-				sp.attachToParent(getMap(), mapChosen);
-				//sp.draw();//
-			} else if (Input.actionJustPressed(controller.getDevice(), Action.UI_DOWN)) {
-				if (init > 0) {
-					init--;
-				}
-				else {
-					init=max-1;
-				}
-				mapChosen = cho[init];
-				getMapChoosen();
+			else{
+				init=0;
 			}
+			mapChosen = cho[init];
+			getMapChoosen();
+			//String n = ;
+			Sprite sp = new Sprite(new Vec2f(0, 0), "data/MAP_VIS/TREE.png");
+			sp.attachToParent(getMap(), mapChosen);
+			//sp.draw();//
+		} else if (Input.actionJustPressed(Device.KEYBOARD, Action.UI_DOWN)) {
+			if (init > 0) {
+				init--;
+			}
+			else {
+				init=max-1;
+			}
+			mapChosen = cho[init];
+			getMapChoosen();
 		}
 
 		if (Input.actionPressed(Device.KEYBOARD, Action.UI_OK)) {
