7a3b3703dc869aa43d0c42bc6b43a1bf497ce9a1
Marz <marinz-muz@hotmail.fr>
2019-03-06 13:58:39 +0100

    previous state

--- project/src/arenashooter/game/GameMaster.java
+++ project/src/arenashooter/game/GameMaster.java
@@ -2,6 +2,7 @@ package arenashooter.game;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Stack;
 
 import arenashooter.entities.Controller;
 import arenashooter.entities.Entity;
@@ -14,61 +15,101 @@ import arenashooter.game.gameStates.MapChooser;
 import arenashooter.game.gameStates.Start;
 
 public class GameMaster {
-	public static final GameMaster gm = new GameMaster();
-	
+
 	public ArrayList<Controller> controllers = new ArrayList<>();
-	
+
+	Stack<GameState> stateStack = new Stack<>();
 	private static GameState current = new Start();
-	
+
+	public static final GameMaster gm = new GameMaster();
+
 	private GameMaster() {
 		// Constructor untouchable
 	}
-	
+
 	public void requestNextState() {
-		if(current instanceof Start) { //Start
-			Loading.loading.setNextState(new Intro(), "data/mapXML/empty.xml");// TODO : create the map
-			current = Loading.loading;
-			current.init();
-		} else if (current instanceof CharacterChooser) { //Character chooser
-			CharacterChooser c = (CharacterChooser) current;
-			for (Controller controller : c.getControllers())
-				controllers.add(controller);
-			
-			//Loading.loading.setNextState(new Game(),"data/mapXML/mapXML.xml");// TODO : create the map
-			Loading.loading.setNextState(new MapChooser(), "data/mapXML/empty.xml");
-			current = Loading.loading;
-			current.init();
-		} else if (current instanceof MapChooser) { //Map chooser
-			MapChooser mapChooser = (MapChooser) current;
-			current = Loading.loading;
-			current.init();
-			Loading.loading.setNextState(new Game(), mapChooser.getMapChoosen());// TODO : create the map
-		} else if (current == Loading.loading) { //Loading
+
+		if (current == Loading.loading) { // Loading
 			current = Loading.loading.getNextState();
-		} else if (current instanceof Intro) { //Intro movie
-			System.out.println("mlep");
-			current = Loading.loading;
-			current.init();
-			Loading.loading.setNextState(new CharacterChooser(), "data/mapXML/empty.xml");// TODO : create the map
+		} else {
+			stateStack.push(current);
+			if (current instanceof Start) { // Start
+				Loading.loading.setNextState(new Intro(), "data/mapXML/empty.xml");// TODO : create the map
+				current = Loading.loading;
+				current.init();
+			} else if (current instanceof CharacterChooser) { // Character chooser
+				CharacterChooser c = (CharacterChooser) current;
+				controllers.clear();
+				for (Controller controller : c.getControllers())
+					controllers.add(controller);
+
+				// Loading.loading.setNextState(new Game(),"data/mapXML/mapXML.xml");// TODO :
+				// create the map
+				Loading.loading.setNextState(new MapChooser(), "data/mapXML/empty.xml");
+				current = Loading.loading;
+				current.init();
+			} else if (current instanceof MapChooser) { // Map chooser
+				MapChooser mapChooser = (MapChooser) current;
+				current = Loading.loading;
+				current.init();
+				Loading.loading.setNextState(new Game(), mapChooser.getMapChoosen());// TODO : create the map
+			} else if (current instanceof Intro) { // Intro movie
+				System.out.println("mlep");
+				current = Loading.loading;
+				current.init();
+				Loading.loading.setNextState(new CharacterChooser(), "data/mapXML/empty.xml");// TODO : create the map
+			}
 		}
+
+//		if (current instanceof Start) { // Start
+//			Loading.loading.setNextState(new Intro(), "data/mapXML/empty.xml");// TODO : create the map
+//			current = Loading.loading;
+//			current.init();
+//		} else if (current instanceof CharacterChooser) { // Character chooser
+//			CharacterChooser c = (CharacterChooser) current;
+//			for (Controller controller : c.getControllers())
+//				controllers.add(controller);
+//
+//			// Loading.loading.setNextState(new Game(),"data/mapXML/mapXML.xml");// TODO :
+//			// create the map
+//			Loading.loading.setNextState(new MapChooser(), "data/mapXML/empty.xml");
+//			current = Loading.loading;
+//			current.init();
+//		} else if (current instanceof MapChooser) { // Map chooser
+//			MapChooser mapChooser = (MapChooser) current;
+//			current = Loading.loading;
+//			current.init();
+//			Loading.loading.setNextState(new Game(), mapChooser.getMapChoosen());// TODO : create the map
+//		} else if (current == Loading.loading) { // Loading
+//			current = Loading.loading.getNextState();
+//		} else if (current instanceof Intro) { // Intro movie
+//			System.out.println("mlep");
+//			current = Loading.loading;
+//			current.init();
+//			Loading.loading.setNextState(new CharacterChooser(), "data/mapXML/empty.xml");// TODO : create the map
+//		}
 	}
-	
+
+	public void requestPreviousState() {
+		 current = stateStack.pop();
+	}
+
 	/**
 	 * @return An ArrayList of all entities in the current map of the game
 	 */
-	public Collection<Entity> getEntities(){
+	public Collection<Entity> getEntities() {
 		return current.getMap().children.values();
 	}
-	
+
 	public Map getMap() {
-		return current.getMap(); 
+		return current.getMap();
 	}
-	
+
 	public void draw() {
-		if(current.getMap() != null)
+		if (current.getMap() != null)
 			current.draw();
 	}
-	
+
 	public void update(double delta) {
 		current.update(delta);
 	}
