e229213a147f12300783d4a5d2ee271fce487a67
SnPop <shervinpedaran@yahoo.fr>
2019-03-06 13:19:51 +0100

    PAUSE + a good map

--- project/src/arenashooter/game/Game.java
+++ project/src/arenashooter/game/Game.java
@@ -6,32 +6,39 @@ import java.util.Iterator;
 import arenashooter.engine.audio.Audio;
 import arenashooter.engine.graphics.PostProcess;
 import arenashooter.engine.graphics.Window;
+import arenashooter.engine.graphics.fonts.Text;
+import arenashooter.engine.input.Action;
+import arenashooter.engine.input.Device;
+import arenashooter.engine.input.Input;
 import arenashooter.engine.math.Quat;
 import arenashooter.engine.math.Vec3f;
 import arenashooter.entities.Controller;
 import arenashooter.entities.spatials.Camera;
 import arenashooter.entities.spatials.Character;
+import arenashooter.entities.spatials.TextSpatial;
 import arenashooter.game.gameStates.GameState;
 
 public class Game extends GameState {
 	private int nbPlayers = GameMaster.gm.controllers.size();
 	private ArrayList<Character> players = new ArrayList<>(nbPlayers);
 	private Iterator<Controller> iterator = GameMaster.gm.controllers.iterator();
+	private boolean pause = false;
 
-	public Game() { }
-	
+	public Game() {
+	}
+
 	@Override
 	public void init() {
-		
+
 		long time = System.currentTimeMillis();
-		
-		while(iterator.hasNext()) {
+
+		while (iterator.hasNext()) {
 			Controller controller = iterator.next();
 			Character character = controller.createNewCharacter(map.GetRandomRespawn());
 			players.add(character);
 			character.attachToParent(map, character.genName());
 		}
-		
+
 		Window.postProcess = new PostProcess("data/shaders/post_process/pp_default");
 		Window.camera = new Camera(new Vec3f(0, 0, 450));
 		Window.camera.attachToParent(map, "camera");
@@ -46,10 +53,26 @@ public class Game extends GameState {
 		} else
 			Audio.setListener(new Vec3f(), Quat.fromAngle(0));
 
-		//Update controllers
-		for(Controller controller : GameMaster.gm.controllers)
+		// Update controllers
+		for (Controller controller : GameMaster.gm.controllers)
 			controller.step(d);
-			
-		map.step(d);
+
+		if (Input.actionJustPressed(Device.KEYBOARD, Action.UI_BACK)) {
+			pause = !pause;
+		}
+		if (!pause) {
+			if (map.children.get("Text_Pause") != null) {
+				map.children.get("Text_Pause").destroy();
+			}
+			map.step(d);
+		} else {
+			Text text = new Text(Main.font, Text.TextAlignH.CENTER, "PAUSE");
+			TextSpatial textEnt = new TextSpatial(Window.camera.pos(), new Vec3f(450), text);
+			textEnt.position.z = 0;
+			textEnt.localPosition.z = 0;
+			textEnt.zIndex = 9999;
+			textEnt.attachToParent(map, "Text_Pause");
+		}
+
 	}
 }
