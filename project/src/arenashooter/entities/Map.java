86659f6986c3cde0b867cf6d94f1c84495431f05
Kapouett <kapouett@orange.fr>
2019-05-10 16:17:15 +0200

    Removed Map.init() and moved call to createTestEntities()

--- project/src/arenashooter/entities/Map.java
+++ project/src/arenashooter/entities/Map.java
@@ -41,6 +41,7 @@ public class Map extends Entity {
 
 	public Map() {
 		physic = new Physic(this);
+		createTestEntities();
 	}
 
 	@Override
@@ -168,33 +169,5 @@ public class Map extends Entity {
 		
 		AnimationTester animTester = new AnimationTester(new Vec2f(500, 0));
 		animTester.attachToParent(this, "anim tester 1");
-	}
-
-	// déso, ces lignes sont temporaires
-	public void init() {
-		createTestEntities();
-		spawnWeapon.attachToParent(this, "test");
-
-		// Gun gun2 = new Gun(new Vec2f(300, 350));
-		//// Melee kata = new Melee(new Vec2f(750, 350));
-		// Gun gun3 = new Gun(new Vec2f(-250, 1050), "data/weapons/Minigun_1.png",
-		// "Bang2", "GunCock1", "GunCock1", "jump",
-		// 0.03d, 0, 4000f, 5, 65d, 0.3, 500, 1, 1);
-		// Gun gun4 = new Gun(new Vec2f(1000, 350), "data/weapons/Minigun_1.png",
-		// "Bang2", "GunCock1", "GunCock1", "jump",
-		// 0.03d, 0, 4000f, 5, 65d, 0.3, 500, 1, 1);
-		// Gun gun5 = new Gun(new Vec2f(1000, 1050), "data/weapons/Minigun_1.png",
-		// "Bang2", "GunCock1", "GunCock1", "jump",
-		// 0.03d, 0, 4000f, 5, 65d, 0.3, 500, 1, 1);
-		// Gun gun6 = new Gun(new Vec2f(1000, 1450), "data/weapons/RayGun1_tr.png",
-		// "BangIonGun2", "GunCock1",
-		// "IonChargeV2_3", "jump", 0.10d, 1, 2500f, 5, 55d, 0.4, 200, 0.6, 3);
-		// gun6.attachToParent(this, "Item_Weapon" + genName());
-		//// kata.attachToParent(this, "Item_Weapon"+genName());
-		// gun2.attachToParent(this, "Item_Weapon" + genName());
-		// gun3.attachToParent(this, "Item_Weapon" + genName());
-		// gun4.attachToParent(this, "Item_Weapon" + genName());
-		// gun5.attachToParent(this, "Item_Weapon" + genName());
-
 	}
 }
