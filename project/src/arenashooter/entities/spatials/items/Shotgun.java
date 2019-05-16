66e1704c82076272597dab7917dd4ff111e2d984
Marz <marinz-muz@hotmail.fr>
2019-05-16 11:52:13 +0200

    Shotgun has a minimum of 4 projectiles that are 4x more precise.

--- project/src/arenashooter/entities/spatials/items/Shotgun.java
+++ project/src/arenashooter/entities/spatials/items/Shotgun.java
@@ -42,121 +42,139 @@ public class Shotgun extends Gun {
 	 */
 	public Shotgun(Vec2f position, String name, double weight, String pathSprite, Vec2f handPosL, Vec2f handPosR,
 			String soundPickup, double cooldown, int uses, String animPath, double warmupDuration, String soundWarmup,
-			String bangSound, String noAmmoSound, int multiShot, double dispersion, int bulletType, float bulletSpeed, float damage,
-			double cannonLength, double recoil, double thrust, double size) {
-		
+			String bangSound, String noAmmoSound, int multiShot, double dispersion, int bulletType, float bulletSpeed,
+			float damage, double cannonLength, double recoil, double thrust, double size) {
+
 		super(position, name, weight, pathSprite, handPosL, handPosR, soundPickup, cooldown, uses, animPath,
-				warmupDuration, soundWarmup, bangSound, noAmmoSound, bulletType, bulletSpeed, damage, cannonLength, recoil, thrust,
-				size);
+				warmupDuration, soundWarmup, bangSound, noAmmoSound, bulletType, bulletSpeed, damage, cannonLength,
+				recoil, thrust, size);
 		this.multiShot = multiShot;
 		this.dispersion = dispersion;
 	}
 
 	@Override
-		public void step(double d) {
-			if (sndWarmup != null) {
-				if (timerWarmup.isIncreasing()) {
-					sndChargeVol = Utils.lerpF(sndChargeVol, 0.20f, d * 15);
-					sndChargePitch = Utils.lerpF(sndChargePitch, 3.5f, d * 4.5);
-				} else {
-					sndChargeVol = Utils.lerpF(sndChargeVol, 0, d * .04);
-					sndChargePitch = Utils.lerpF(sndChargePitch, .01f, d * 2.5);
-				}
-				sndWarmup.setVolume(sndChargeVol);
-				if (sndWarmup.getSound() instanceof SoundSourceSingle)
-					((SoundSourceSingle) sndWarmup.getSound()).setPitch(sndChargePitch);
+	public void step(double d) {
+		if (sndWarmup != null) {
+			if (timerWarmup.isIncreasing()) {
+				sndChargeVol = Utils.lerpF(sndChargeVol, 0.20f, d * 15);
+				sndChargePitch = Utils.lerpF(sndChargePitch, 3.5f, d * 4.5);
+			} else {
+				sndChargeVol = Utils.lerpF(sndChargeVol, 0, d * .04);
+				sndChargePitch = Utils.lerpF(sndChargePitch, .01f, d * 2.5);
 			}
-			
-			//Spawn projectile
-			if (timerWarmup.isIncreasing() && timerWarmup.isOver() && timerCooldown.isOver()) {
-				timerCooldown.restart();
+			sndWarmup.setVolume(sndChargeVol);
+			if (sndWarmup.getSound() instanceof SoundSourceSingle)
+				((SoundSourceSingle) sndWarmup.getSound()).setPitch(sndChargePitch);
+		}
 
-				if (nbAmmo > 0) {
-					Vec2f aim = Vec2f.fromAngle(rotation);
+		// Spawn projectile
+		if (timerWarmup.isIncreasing() && timerWarmup.isOver() && timerCooldown.isOver()) {
+			timerCooldown.restart();
 
-					Vec2f bulSpeed = Vec2f.multiply(aim, bulletSpeed);
-					Vec2f bulletPos = pos();
-					bulletPos.add(Vec2f.multiply(aim, cannonLength));
+			if (nbAmmo > 0) {
+				Vec2f aim = Vec2f.fromAngle(rotation);
 
-					Particles flash;
-					
-					nbAmmo--;
-					
-					
-					switch (bulletType) {
-					case 0:
-						for(int i = 0 ; i<multiShot ;i++) {
-							Bullet bul = new Bullet(bulletPos, Vec2f.multiply((Vec2f.rotate(bulSpeed, (Math.random()-0.5)*2*dispersion)), 1+Math.random()/4), damage);
+				Vec2f bulSpeed = Vec2f.multiply(aim, bulletSpeed);
+				Vec2f bulletPos = pos();
+				bulletPos.add(Vec2f.multiply(aim, cannonLength));
+
+				Particles flash;
+
+				nbAmmo--;
+
+				switch (bulletType) {
+				case 0:
+					for (int i = 0; i < multiShot; i++) {
+
+						if (i > 3) {
+							Bullet bul = new Bullet(bulletPos,
+									Vec2f.multiply((Vec2f.rotate(bulSpeed, (Math.random() - 0.5) * 2 * dispersion)),
+											1 + Math.random() / 4),
+									damage);
+							bul.attachToParent(getMap(), ("bullet_" + bul.genName()));
+							if (isEquipped())
+								bul.shooter = ((Character) parent);
+						} else {
+							Bullet bul = new Bullet(bulletPos,
+									Vec2f.multiply((Vec2f.rotate(bulSpeed, (Math.random() - 0.5) * 0.5 * dispersion)),
+											1 + Math.random() / 4),
+									damage);
 							bul.attachToParent(getMap(), ("bullet_" + bul.genName()));
 							if (isEquipped())
 								bul.shooter = ((Character) parent);
 						}
-						flash = new Particles(bulletPos, "data/particles/flash_01.xml");
-						flash.attachToParent(getChildren().get("particle_container"), "particles_flash");
-						break;
+					}
+					flash = new Particles(bulletPos, "data/particles/flash_01.xml");
+					flash.attachToParent(getChildren().get("particle_container"), "particles_flash");
+					break;
 
-					case 1:
-						flash = new Particles(bulletPos, "data/particles/flash_02.xml");
-						flash.attachToParent(getChildren().get("particle_container"), "particles_flash");
+				case 1:
+					flash = new Particles(bulletPos, "data/particles/flash_02.xml");
+					flash.attachToParent(getChildren().get("particle_container"), "particles_flash");
 
-						for(int i = 0 ; i<multiShot ;i++) {
-							CircleBullet bull = new CircleBullet(bulletPos, Vec2f.multiply((Vec2f.rotate(bulSpeed, (Math.random()-0.5)*2*dispersion)), 1+Math.random()/4), damage, false);
-							CircleBullet bull2 = new CircleBullet(bulletPos, Vec2f.multiply((Vec2f.rotate(bulSpeed, (Math.random()-0.5)*2*dispersion)), 1+Math.random()/4), damage, true);
+					for (int i = 0; i < multiShot; i++) {
+						CircleBullet bull = new CircleBullet(bulletPos,
+								Vec2f.multiply((Vec2f.rotate(bulSpeed, (Math.random() - 0.5) * 2 * dispersion)),
+										1 + Math.random() / 4),
+								damage, false);
+						CircleBullet bull2 = new CircleBullet(bulletPos,
+								Vec2f.multiply((Vec2f.rotate(bulSpeed, (Math.random() - 0.5) * 2 * dispersion)),
+										1 + Math.random() / 4),
+								damage, true);
 
-							if (isEquipped()) {
-								bull.shooter = ((Character) parent);
-								bull2.shooter = ((Character) parent);
-							}
-							bull.attachToParent(getMap(), ("bullet_" + bull.genName()));
-							bull2.attachToParent(getMap(), ("bullet_" + bull2.genName()));
+						if (isEquipped()) {
+							bull.shooter = ((Character) parent);
+							bull2.shooter = ((Character) parent);
 						}
-						break;
-
-					default:
-
-						Bullet bul1 = new Bullet(bulletPos, bulSpeed, damage);
-						bul1.attachToParent(getMap(), ("bullet_" + bul1.genName()));
-						if (isEquipped())
-							bul1.shooter = ((Character) parent);
-						flash = new Particles(bulletPos, "data/particles/flash_01.xml");
-						flash.attachToParent(getChildren().get("particle_container"), "particles_flash");
-
-						break;
+						bull.attachToParent(getMap(), ("bullet_" + bull.genName()));
+						bull2.attachToParent(getMap(), ("bullet_" + bull2.genName()));
 					}
-					if (isEquipped()) {
-						getVel().add(Vec2f.multiply(Vec2f.rotate(aim, Math.PI), recoil * 5000));
-						((Character) parent).vel.add(Vec2f.multiply(Vec2f.rotate(aim, Math.PI), thrust));
-					} else {
-						getVel().add(Vec2f.multiply(Vec2f.rotate(aim, Math.PI), thrust / 10));
-					}
+					break;
 
-					((SoundEffect) getChildren().get("snd_Bang")).play();
+				default:
 
-					Particles shell = new Particles(bulletPos, "data/particles/shell_01.xml");
-					shell.selfDestruct = true;
-					shell.attachToParent(this, shell.genName());
+					Bullet bul1 = new Bullet(bulletPos, bulSpeed, damage);
+					bul1.attachToParent(getMap(), ("bullet_" + bul1.genName()));
+					if (isEquipped())
+						bul1.shooter = ((Character) parent);
+					flash = new Particles(bulletPos, "data/particles/flash_01.xml");
+					flash.attachToParent(getChildren().get("particle_container"), "particles_flash");
 
-					rotation += ((Math.random()) - 0.5) * recoil;
-					
-					// Add camera shake
-					Window.getCamera().setCameraShake(2.8f);
+					break;
+				}
+				if (isEquipped()) {
+					getVel().add(Vec2f.multiply(Vec2f.rotate(aim, Math.PI), recoil * 5000));
+					((Character) parent).vel.add(Vec2f.multiply(Vec2f.rotate(aim, Math.PI), thrust));
 				} else {
-					((SoundEffect) getChildren().get("snd_NoAmmo")).play();
+					getVel().add(Vec2f.multiply(Vec2f.rotate(aim, Math.PI), thrust / 10));
 				}
 
+				((SoundEffect) getChildren().get("snd_Bang")).play();
+
+				Particles shell = new Particles(bulletPos, "data/particles/shell_01.xml");
+				shell.selfDestruct = true;
+				shell.attachToParent(this, shell.genName());
+
+				rotation += ((Math.random()) - 0.5) * recoil;
+
+				// Add camera shake
+				Window.getCamera().setCameraShake(2.8f);
+			} else {
+				((SoundEffect) getChildren().get("snd_NoAmmo")).play();
 			}
 
-			getSprite().rotation = rotation;
-
-			super.step(d);
 		}
 
+		getSprite().rotation = rotation;
+
+		super.step(d);
+	}
 
 	@Override
 	public Shotgun clone(Vec2f position) {
-		Shotgun gun = new Shotgun(position, this.genName(), weight, pathSprite, handPosL, handPosR, soundPickup, fireRate, uses,
-				animPath, warmupDuration, soundWarmup, bangSound, noAmmoSound, multiShot, dispersion, bulletType, bulletSpeed, damage,
-				cannonLength, recoil, thrust, size);
+		Shotgun gun = new Shotgun(position, this.genName(), weight, pathSprite, handPosL, handPosR, soundPickup,
+				fireRate, uses, animPath, warmupDuration, soundWarmup, bangSound, noAmmoSound, multiShot, dispersion,
+				bulletType, bulletSpeed, damage, cannonLength, recoil, thrust, size);
 		return gun;
 	}
 }
