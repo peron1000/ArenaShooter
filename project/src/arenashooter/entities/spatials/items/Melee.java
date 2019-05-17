b0c15e3fff1d9f74479404688fdb805f4e481328
peron1000 <pmot.repon@free.fr>
2019-05-17 10:20:51 +0200

    melee fix

--- project/src/arenashooter/entities/spatials/items/Melee.java
+++ project/src/arenashooter/entities/spatials/items/Melee.java
@@ -2,6 +2,8 @@ package arenashooter.entities.spatials.items;
 
 import arenashooter.engine.math.Vec2f;
 import arenashooter.entities.Timer;
+import arenashooter.entities.spatials.AnimationTester;
+import arenashooter.entities.spatials.Character;
 import arenashooter.entities.spatials.Collider;
 import arenashooter.entities.spatials.SoundEffect;
 
@@ -11,6 +13,8 @@ public class Melee extends Usable {
 	protected float damage = 10f;
 	/** Time before the first bullet is fired */
 	protected Timer timerWarmup = null;
+	
+	protected animMelee animmelee = null;
 
 	public Melee(Vec2f position, String name, double weight, String pathSprite, Vec2f handPosL, Vec2f handPosR,
 			String soundPickup, double cooldown, int uses, String animPath, double warmupDuration, String soundWarmup,
@@ -18,14 +22,14 @@ public class Melee extends Usable {
 		super(position, name, weight, pathSprite, handPosL, handPosR, soundPickup, cooldown, uses, animPath,
 				warmupDuration, soundWarmup, attackSound);
 
-		SoundEffect pickup = new SoundEffect(position, "data/sound/" + soundPickup + ".ogg", 2);
+		/*SoundEffect pickup = new SoundEffect(position, "data/sound/" + soundPickup + ".ogg", 2);
 		pickup.attachToParent(this, "snd_Pickup");
 
 		SoundEffect attack = new SoundEffect(position, "data/sound/" + attackSound + ".ogg", 2);
 		attack.attachToParent(this, "snd_attack");
 
 		SoundEffect warmup = new SoundEffect(position, "data/sound/" + soundWarmup + ".ogg", 2);
-		warmup.attachToParent(this, "snd_Warmup");
+		warmup.attachToParent(this, "snd_Warmup");*/
 		
 		// Warmup
 		this.timerWarmup = new Timer(warmupDuration);
@@ -39,6 +43,9 @@ public class Melee extends Usable {
 		this.timerCooldown.setProcessing(true);
 		this.timerCooldown.setValue(cooldown);
 		this.timerCooldown.attachToParent(this, "timer_cooldown");
+		
+		this.animmelee = new animMelee(new Vec2f(0, 0), this);
+		animmelee.attachToParent(this, "anim fzfzef 1");
 	}
 
 	@Override
@@ -55,15 +62,9 @@ public class Melee extends Usable {
 	public void step(double d) {
 		if (timerWarmup.isIncreasing() && timerWarmup.isOver() && timerCooldown.isOver()) {
 			timerCooldown.restart();
-			Vec2f aim = Vec2f.fromAngle(rotation);
-			this.position = aim;
+			animmelee.playAnim();
 		}
 		super.step(d);
-	}
-
-	@Override
-	protected void setLocalPositionOfSprite() {
-		localPosition = Vec2f.rotate(new Vec2f(20, 0), rotation);
 	}
 
 	@Override
