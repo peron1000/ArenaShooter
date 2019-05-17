8d3bf37c5047ecb6c7147de89a1c6a438413ed82
Marz <marinz-muz@hotmail.fr>
2019-05-17 13:16:56 +0200

    Tableau de score à la fin du nbRounds (1 round de base)

--- project/src/arenashooter/game/gameStates/Score.java
+++ project/src/arenashooter/game/gameStates/Score.java
@@ -8,18 +8,24 @@ import arenashooter.engine.input.Device;
 import arenashooter.engine.input.Input;
 import arenashooter.engine.math.Vec2f;
 import arenashooter.engine.math.Vec4f;
+import arenashooter.engine.ui.Menu;
 import arenashooter.engine.ui.UiImage;
 import arenashooter.entities.Controller;
 import arenashooter.game.GameMaster;
 
 public class Score extends GameState {
+	
 	@Override
 	public void init(){
+		menu = new Menu();
+		
 		Texture texture1 = Texture.loadTexture("data/sprites/interface/Fond Menu.png");
 		texture1.setFilter(false);
 
 		Texture texture2 = Texture.loadTexture("data/sprites/interface/Selector.png");
 		texture2.setFilter(false);
+		
+		new UiImage(menu, new Vec2f(), 0, new Vec2f(177.78, 100), texture1, new Vec4f(1, 1, 1, 1));
 		
 		super.init();
 	}
