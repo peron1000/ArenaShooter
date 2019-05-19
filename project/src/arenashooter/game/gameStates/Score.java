b636bbd6f68de2ed6d2adb56b1b6852427d87a0d
Marz <marinz-muz@hotmail.fr>
2019-05-19 16:14:21 +0200

    ButtonsABXY Added

--- project/src/arenashooter/game/gameStates/Score.java
+++ project/src/arenashooter/game/gameStates/Score.java
@@ -19,7 +19,7 @@ public class Score extends GameState {
 	public void init(){
 		menu = new Menu();
 		
-		Texture texture1 = Texture.loadTexture("data/sprites/interface/Fond Score.png");
+		Texture texture1 = Texture.loadTexture("data/sprites/interface/Fond Menu_Score.png");
 		texture1.setFilter(false);
 
 		Texture texture2 = Texture.loadTexture("data/sprites/interface/Selector.png");
