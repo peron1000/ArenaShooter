a1f51539de3433f16c106e4188261eb744340e5d
Kapouett <kapouett@orange.fr>
2019-05-22 14:01:25 +0200

    Pushed far clip plane to 4500

--- project/src/arenashooter/engine/graphics/Window.java
+++ project/src/arenashooter/engine/graphics/Window.java
@@ -62,7 +62,7 @@ public final class Window {
 	/** Perspective projection matrix */
 	public static Mat4f proj;
 	private static float fov = 70;
-	private static final float CLIP_NEAR = 2, CLIP_FAR = 2500;
+	private static final float CLIP_NEAR = 2, CLIP_FAR = 4500;
 	/** Orthographic projection matrix */
 	public static Mat4f projOrtho;
 	
