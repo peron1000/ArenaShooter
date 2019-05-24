a56f7752010b9f1a9509e1491d3c0e191eff22b7
SnPop <shervinpedaran@yahoo.fr>
2019-05-24 16:14:14 +0200

    menuPause Ultimate

--- project/src/arenashooter/engine/ui/MenuPause.java
+++ project/src/arenashooter/engine/ui/MenuPause.java
@@ -6,7 +6,12 @@ import java.util.LinkedList;
 import org.apache.logging.log4j.core.util.Log4jThread;
 
 import arenashooter.engine.events.BooleanProperty;
+import arenashooter.engine.events.EventListener;
+import arenashooter.engine.events.input.InputActionEvent;
+import arenashooter.engine.events.input.InputListener;
 import arenashooter.engine.input.Action;
+import arenashooter.engine.input.ActionState;
+import arenashooter.engine.input.ActionTest;
 import arenashooter.engine.input.Device;
 import arenashooter.engine.input.Input;
 import arenashooter.engine.math.Vec2f;
@@ -21,10 +26,10 @@ import arenashooter.game.gameStates.Intro;
 public class MenuPause extends MenuSelectionV<Label> {
 
 	private Label op1, op2, op3, op4;
-	private boolean activated = false;
+	private InputListener inputs = new InputListener();
 
-	public MenuPause(float x , float y) {
-		super(5 , x , y ,new Vec2f(30 ,10), "data/sprites/interface/Selector.png");
+	public MenuPause(float x, float y) {
+		super(5, x, y, new Vec2f(30, 10), "data/sprites/interface/Selector.png");
 		if (maxLayout < 3) {
 			Exception e = new Exception("Max layout trop petit");
 			e.printStackTrace();
@@ -34,14 +39,44 @@ public class MenuPause extends MenuSelectionV<Label> {
 				elems.put(Integer.valueOf(i), new LinkedList<>());
 			}
 		}
+		inputs.actions.add(new EventListener<InputActionEvent>() {
+
+			@Override
+			public void action(InputActionEvent event) {
+				// TODO Auto-generated method stub
+				if (event.getActionState() == ActionState.JUST_PRESSED) {
+					switch (event.getAction()) {
+					case UI_OK:
+						getTarget().lunchAction("ok");
+						break;
+
+					case UI_PAUSE:
+						active =  new BooleanProperty(false);
+						break;
+					case UI_BACK:
+						active =  new BooleanProperty(false);
+						break;
+					case UI_UP:
+						up();
+						break;
+					case UI_DOWN:
+						down();
+						break;
+					default:
+						break;
+					}
+
+				}
+			}
+		});
 		final float scale = 24;
-		setPositionRef(new Vec2f(getPosition().x, getPosition().y-12.5));
+		setPositionRef(new Vec2f(getPosition().x, getPosition().y - 12.5));
 		setEcartement(10);
 		Rectangle rec = new Rectangle(0, new Vec2f(45, 60), new Vec4f(0, 0, 0, .25));
 		setBackground(rec);
 		Label pause = new Label(0, new Vec2f(50, 50), "PAUSE");
 		addUiElement(pause, 0);
-		pause.setPos(new Vec2f(x, y-27));
+		pause.setPos(new Vec2f(x, y - 27));
 
 		op1 = new Label(0, new Vec2f(scale), "Resume");
 		addElementInListOfChoices(op1, 1);
@@ -54,78 +89,48 @@ public class MenuPause extends MenuSelectionV<Label> {
 
 		op4 = new Label(0, new Vec2f(scale), "Quit : Alt+f4");
 		addElementInListOfChoices(op4, 1);
-
+		
 		op1.addAction("ok", new Trigger() {
-
 			@Override
 			public void make() {
 				try {
-//					Robot robot = new Robot();
-//					robot.keyPress(java.awt.event.KeyEvent.VK_ESCAPE);
-					active =  new BooleanProperty(false);
+					Robot robot = new Robot();
+					robot.keyPress(java.awt.event.KeyEvent.VK_ESCAPE);
+//					active = new BooleanProperty(false);
 					System.out.println("op1 : Resume");
 				} catch (Exception ex) {
 					System.out.println("fail pause");
 					Log4jThread.getDefaultUncaughtExceptionHandler();
-
 				}
 			}
 		});
 		op2.addAction("ok", new Trigger() {
-
 			@Override
 			public void make() {
 				System.out.println("op2 : Score");
 			}
 		});
 		op3.addAction("ok", new Trigger() {
-
 			@Override
 			public void make() {
 				System.out.println("op3 : Option");
 			}
 		});
 		op4.addAction("ok", new Trigger() {
-
 			@Override
 			public void make() {
 				System.out.println("op4 : quit");
 				GameMaster.gm.requestNextState(new Intro(), GameMaster.mapEmpty);
 			}
 		});
-
 		op3.visible = false;
 	}
 
 	@Override
 	public void update(double delta) {
 		// Detect controls
-		for (Controller controller : GameMaster.gm.controllers) {
-			if (Input.actionJustPressed(controller.getDevice(), Action.UI_UP)) {
-				if (!activated) {
-					this.up();
-				}
-			} else if (Input.actionJustPressed(controller.getDevice(), Action.UI_DOWN)) {
-				if (!activated) {
-					this.down();
-				}
-			} else if (Input.actionJustPressed(Device.KEYBOARD, Action.JUMP)) {
-				activated = !activated;
-			} else if (Input.actionJustPressed(Device.KEYBOARD, Action.UI_OK)) {
-				this.getTarget().lunchAction("ok");
-
-			} else if (Input.actionJustPressed(controller.getDevice(), Action.JUMP)) {
-				this.getTarget().lunchAction("ok");
-			}
-			 else if (Input.actionJustPressed(controller.getDevice(), Action.UI_PAUSE)) {
-					active =  new BooleanProperty(false);
-					//System.out.println("uipause");
-				}
-		}
-		super.update(delta);
+		inputs.step(delta);
 		
-
-
+		super.update(delta);
 	}
-
 }
