b1ac999ed7140aaeef614a3e60b44be2ed83db5e
SnPop <shervinpedaran@yahoo.fr>
2019-05-24 16:42:00 +0200

    intro avec les nouveaux inputs

--- project/src/arenashooter/game/gameStates/Intro.java
+++ project/src/arenashooter/game/gameStates/Intro.java
@@ -1,7 +1,12 @@
 package arenashooter.game.gameStates;
 
 import arenashooter.engine.animation.AnimIntro;
+import arenashooter.engine.events.BooleanProperty;
+import arenashooter.engine.events.EventListener;
+import arenashooter.engine.events.input.InputActionEvent;
+import arenashooter.engine.events.input.InputListener;
 import arenashooter.engine.input.Action;
+import arenashooter.engine.input.ActionState;
 import arenashooter.engine.input.Device;
 import arenashooter.engine.input.Input;
 import arenashooter.entities.Entity;
@@ -9,10 +14,31 @@ import arenashooter.entities.Music;
 import arenashooter.game.GameMaster;
 
 public class Intro extends GameState {
-	public Intro() { 
+	private InputListener inputs = new InputListener();
+
+	public Intro() {
 		super(1);
+		inputs.actions.add(new EventListener<InputActionEvent>() {
+			@Override
+			public void action(InputActionEvent event) {
+				// TODO Auto-generated method stub
+				if (event.getActionState() == ActionState.JUST_PRESSED) {
+					switch (event.getAction()) {
+					case UI_CONTINUE:
+						Entity bgm = current.getChild("bgm");
+						if (bgm instanceof Music)
+							((Music) bgm).stop();
+						GameMaster.gm.requestNextState(new Config(), "data/mapXML/menu_empty.xml");
+						break;
+					default:
+						break;
+					}
+
+				}
+			}
+		});
 	}
-	
+
 	@Override
 	public void init() {
 		super.init();
@@ -21,13 +47,9 @@ public class Intro extends GameState {
 
 	@Override
 	public void update(double delta) {
+		// Detect controls
+		inputs.step(delta);
 		current.step(delta);
-		if(Input.actionJustPressed(Device.KEYBOARD, Action.UI_OK)||Input.actionJustPressed(Device.CONTROLLER01, Action.UI_OK)) {
-			Entity bgm = current.getChild("bgm");
-			if(bgm instanceof Music) ((Music)bgm).stop();
-			
-			GameMaster.gm.requestNextState(new Config(), "data/mapXML/menu_empty.xml");
-		}
 	}
 
 }
