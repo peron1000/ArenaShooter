9695b85616ae64377bab4014c4fbbbb1ea4489b5
Kapouett <kapouett@orange.fr>
2019-06-03 16:25:28 +0200

    Added the Slaughterhouse

--- project/src/arenashooter/entities/spatials/Camera.java
+++ project/src/arenashooter/entities/spatials/Camera.java
@@ -13,7 +13,7 @@ public class Camera extends Spatial3 {
 	public Mat4f viewMatrix = Mat4f.identity();
 	private Vec3f targetLoc;
 	private Vec2f margin = new Vec2f(5, 5);
-	private float zoomMin = 25;
+	private float zoomMin = 18;
 	
 	/** Vertical field of view */
 	private float fov = 55;
