1bc2319875183036411d86e87f42df7b17d07a6c
Kapouett <kapouett@orange.fr>
2019-06-04 17:45:01 +0200

    Repaired AnimEditorTimeline

--- project/src/arenashooter/engine/ui/AnimEditorTimeline.java
+++ project/src/arenashooter/engine/ui/AnimEditorTimeline.java
@@ -1,16 +1,34 @@
 package arenashooter.engine.ui;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import arenashooter.engine.math.Vec2f;
 import arenashooter.engine.math.Vec4f;
 import arenashooter.engine.ui.simpleElement.Rectangle;
+import arenashooter.engine.ui.simpleElement.UiImage;
+import arenashooter.engine.util.Tuple;
+import arenashooter.game.Main;
 
 public class AnimEditorTimeline extends UiElement {
 	Rectangle bg;
+	
+	double timeScale = 1;
+	double timeOffset = 0;
+	
+	private Map<String, Track> tracksMap = new HashMap<>();
+	private List<String> tracks = new ArrayList<>();
 
 	public AnimEditorTimeline(double rot, Vec2f scale) {
 		super(rot, scale);
 		
 		bg = new Rectangle(rot, scale, new Vec4f(0,0,0, .8f));
+	}
+	
+	public double maxVisibleTime() {
+		return timeOffset+(getScale().x * timeScale);
 	}
 	
 	@Override
@@ -30,10 +48,43 @@ public class AnimEditorTimeline extends UiElement {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public void addTrack(String name) {
+		tracks.add(name);
+		tracksMap.put(name, new Track());
+	}
+	
+	public void removeTrack(String name) {
+		if(!tracks.contains(name) || !tracksMap.containsKey(name)) {
+			Main.log.error("Animation editor tried to delete a non existant track");
+			return;
+		}
+		tracks.remove(name);
+		tracksMap.remove(name);
+	}
 
 	@Override
 	public void draw() {
 		bg.draw();
+	}
+	
+	private class Track {
+		List<Tuple<Double, UiImage>> keyframes;
+		
+		Track() {
+			keyframes = new ArrayList<>();
+		}
+
+		void update() {
+			
+		}
+		
+		void draw() {
+			for(Tuple<Double, UiImage> key : keyframes) {
+				if(key.x >= timeOffset && key.x <= maxVisibleTime())
+					key.y.draw();
+			}
+		}
 	}
 
 }
