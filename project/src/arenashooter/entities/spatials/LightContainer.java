2de817aaaeeb55e87e69ce45f78bceb79198bcbb
Kapouett <kapouett@orange.fr>
2019-06-12 21:14:47 +0200

    Added editorDraw to lights

--- project/src/arenashooter/entities/spatials/LightContainer.java
+++ project/src/arenashooter/entities/spatials/LightContainer.java
@@ -1,12 +1,19 @@
 package arenashooter.entities.spatials;
 
 import arenashooter.engine.graphics.Light;
+import arenashooter.engine.graphics.Material;
+import arenashooter.engine.graphics.Model;
+import arenashooter.engine.math.Quat;
 import arenashooter.engine.math.Vec3f;
+import arenashooter.engine.math.Vec4f;
 import arenashooter.entities.Arena;
 import arenashooter.entities.Entity;
 
 public class LightContainer extends Spatial3 {
 	private final Light light;
+	
+	private Mesh editorSprite;
+	private Material editorSpriteMat;
 
 	public LightContainer(Vec3f localPosition, Light light) {
 		super(localPosition);
@@ -46,5 +53,17 @@ public class LightContainer extends Spatial3 {
 		} else { //Directional
 			light.position.set(getWorldRot().forward());
 		}
+	}
+	
+	@Override
+	public void editorDraw() {
+		if(editorSprite == null) {
+			editorSpriteMat = new Material("data/shaders/sprite_simple");
+			editorSprite = Mesh.quad(new Vec3f(), new Quat(), new Vec3f(1), editorSpriteMat);
+		}
+		editorSpriteMat.setParamVec4f("baseColorMod", new Vec4f(light.color.x,light.color.y,light.color.z,1));
+		editorSprite.localPosition.set(getWorldPos());
+		editorSprite.localRotation.set(getWorldRot());
+		editorSprite.draw();
 	}
 }
