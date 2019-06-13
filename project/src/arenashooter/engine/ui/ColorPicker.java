b27c502dcb5d7e901cb90101e0c1ff3c4ae81569
Kapouett <kapouett@orange.fr>
2019-06-13 16:18:31 +0200

    Better color picker

--- project/src/arenashooter/engine/ui/ColorPicker.java
+++ project/src/arenashooter/engine/ui/ColorPicker.java
@@ -11,6 +11,8 @@ public class ColorPicker extends UiElement {
 
 	private static enum selection {LUMISAT, HUE, ALPHA};
 	
+	private float speed = .05f;
+	
 	private UiImage background = new UiImage(new Vec4f(0, 0, 0, 0.8));
 	
 	private UiImage lumiSat, hueImg, lumiSatSelector, hueSelector, alphaSelector;
@@ -35,12 +37,16 @@ public class ColorPicker extends UiElement {
 		Material lumiSatMat = new Material("data/shaders/ui/ui_gradient_4");
 		lumiSat = new UiImage(lumiSatMat);
 		
-		lumiSat.setPosition(-2.5, 0);
-		
 		hueImg = new UiImage(Texture.loadTexture("data/sprites/interface/hue.png"));
-		hueImg.setRotation(Math.PI/2);
-		hueImg.setPosition(10, 0);
+		hueImg.setRotation(-Math.PI/2);
+		hueImg.setPosition(6, 0);
 		hueImg.setScale(10, 2);
+		
+		lumiSatSelector = new UiImage( Texture.loadTexture("data/sprites/interface/selector_round.png").setFilter(true) );
+		lumiSatSelector.setScale(2.5, 2.5);
+		
+		hueSelector = new UiImage(new Vec4f(1));
+		hueSelector.setScale(hueImg.getScale().y+.2, .45);
 		
 		updateMaterials();
 	}
@@ -48,17 +54,20 @@ public class ColorPicker extends UiElement {
 	private void updateMaterials() {
 		Vec3f.hsvToRgb(hue, saturation, value, rgb);
 		
-		lumiSat.getMaterial().setParamVec4f("colorA", new Vec4f(1, 1, 1, alpha));
-		lumiSat.getMaterial().setParamVec4f("colorB", new Vec4f(rgb.x, rgb.y, rgb.z, alpha));
-		lumiSat.getMaterial().setParamVec4f("colorC", new Vec4f(0, 0, 0, alpha));
-		lumiSat.getMaterial().setParamVec4f("colorD", new Vec4f(0, 0, 0, alpha));
+		Vec3f hueColor = new Vec3f();
+		Vec3f.hsvToRgb(hue, 1, 1, hueColor);
+		
+		lumiSat.getMaterial().setParamVec4f("colorA", new Vec4f(1, 1, 1, 1));
+		lumiSat.getMaterial().setParamVec4f("colorB", new Vec4f(hueColor.x, hueColor.y, hueColor.z, 1));
+		lumiSat.getMaterial().setParamVec4f("colorC", new Vec4f(0, 0, 0, 1));
+		lumiSat.getMaterial().setParamVec4f("colorD", new Vec4f(0, 0, 0, 1));
 	}
 	
 	@Override
 	public void setPosition(double x, double y) {
 		super.setPosition(x, y);
 		background.setPosition(x, y);
-		hueImg.setPosition(x+10, y);
+		hueImg.setPosition(x+3.75, y);
 		lumiSat.setPosition(x-2.5, y);
 	}
 
@@ -78,13 +87,13 @@ public class ColorPicker extends UiElement {
 	public boolean upAction() {
 		switch(current) {
 		case LUMISAT:
-			value += .1;
+			value += speed;
 			break;
 		case HUE:
-			hue += .1;
+			hue += speed;
 			break;
 		case ALPHA:
-			alpha += .1;
+			alpha += speed;
 			break;
 		}
 		return true;
@@ -94,13 +103,13 @@ public class ColorPicker extends UiElement {
 	public boolean downAction() {
 		switch(current) {
 		case LUMISAT:
-			value -= .1;
+			value -= speed;
 			break;
 		case HUE:
-			hue -= .1;
+			hue -= speed;
 			break;
 		case ALPHA:
-			alpha -= .1;
+			alpha -= speed;
 			break;
 		}
 		return true;
@@ -110,7 +119,7 @@ public class ColorPicker extends UiElement {
 	public boolean leftAction() {
 		switch(current) {
 		case LUMISAT:
-			saturation -= .1;
+			saturation -= speed;
 			break;
 		case HUE:
 			current = selection.LUMISAT;
@@ -126,7 +135,7 @@ public class ColorPicker extends UiElement {
 	public boolean rightAction() {
 		switch(current) {
 		case LUMISAT:
-			saturation += .1;
+			saturation += speed;
 			break;
 		case HUE:
 			current = hasAlpha ? selection.ALPHA : selection.LUMISAT;
@@ -178,6 +187,19 @@ public class ColorPicker extends UiElement {
 			background.draw();
 			lumiSat.draw();
 			hueImg.draw();
+
+			if(current == selection.LUMISAT)
+				lumiSatSelector.getMaterial().setParamVec4f("color", new Vec4f(1, 1, .2, 1));
+			else
+				lumiSatSelector.getMaterial().setParamVec4f("color", new Vec4f(1, 1, 1, .7));
+			
+			if(current == selection.HUE)
+				hueSelector.getMaterial().setParamVec4f("color", new Vec4f(1, 1, .2, 1));
+			else
+				hueSelector.getMaterial().setParamVec4f("color", new Vec4f(1, 1, 1, .7));
+			
+			hueSelector.draw();
+			lumiSatSelector.draw();
 		}
 	}
 
@@ -188,6 +210,20 @@ public class ColorPicker extends UiElement {
 		value = Utils.clampF(value, 0, 1); //Remove this clamp?
 		saturation = Utils.clampF(saturation, 0, 1);
 		alpha = hasAlpha ? Utils.clampF(alpha, 0, 1) : 1;
+		
+		float selectorX = lumiSat.getPosition().x;
+		float selectorY = lumiSat.getPosition().y;
+		float halfWidth = lumiSat.getScale().x/2;
+		float halfHeight = lumiSat.getScale().y/2;
+		selectorX = Utils.lerpF(selectorX-halfWidth, selectorX+halfWidth, saturation);
+		selectorY = Utils.lerpF(selectorY+halfHeight, selectorY-halfHeight, value);
+		lumiSatSelector.setPosition(selectorX, selectorY);
+
+		selectorY = hueImg.getPosition().y;
+		halfHeight = hueImg.getScale().x/2; //hueImg is rotated so we use scale.x
+		selectorY = Utils.lerpF(selectorY+halfHeight, selectorY-halfHeight, hue);
+		hueSelector.setPosition(hueImg.getPosition().x, selectorY);
+		
 		updateMaterials();
 	}
 	
