2cfe96a7c11753267bf72c35d3077efbb7f8c0d8
Kapouett <kapouett@orange.fr>
2019-06-13 14:44:29 +0200

    Added LightType

--- project/src/arenashooter/engine/graphics/Light.java
+++ project/src/arenashooter/engine/graphics/Light.java
@@ -3,6 +3,8 @@ package arenashooter.engine.graphics;
 import arenashooter.engine.math.Vec3f;
 
 public class Light {
+	public static enum LightType { POINT, DIRECTIONAL };
+	
 	/** Position of this light, or direction for directional lights */
 	public final Vec3f position = new Vec3f();
 	/** Radius of this light, <0 means this is a directional light */
@@ -10,4 +12,30 @@ public class Light {
 	public final Vec3f color = new Vec3f();
 	
 	public Light() { }
+	
+	/**
+	 * @return type of light (point or directional)
+	 */
+	public LightType getType() {
+		if(radius < 0)
+			return LightType.DIRECTIONAL;
+		return LightType.POINT;
+	}
+	
+	/**
+	 * Set the type of this light (changes radius)
+	 * @param type
+	 */
+	public void setType(LightType type) {
+		switch(type) {
+		case POINT:
+			radius = Math.max(0, radius);
+			break;
+		case DIRECTIONAL:
+			radius = -1;
+			break;
+		default:
+			break;
+		}
+	}
 }
