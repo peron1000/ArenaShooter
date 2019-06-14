f4eea4037bbcdc31566ca7cce414516d4bf43b5c
SnPop <shervinpedaran@yahoo.fr>
2019-06-14 15:24:03 +0200

    barrel + spawner

--- project/src/arenashooter/entities/Arena.java
+++ project/src/arenashooter/entities/Arena.java
@@ -13,45 +13,53 @@ import arenashooter.engine.math.Vec2f;
 import arenashooter.engine.math.Vec3f;
 import arenashooter.engine.math.Vec4f;
 import arenashooter.engine.physic.PhysicWorld;
+import arenashooter.entities.spatials.Barrel;
 import arenashooter.entities.spatials.items.Item;
 import arenashooter.game.Main;
 
 public class Arena extends Entity {
 	/** Character spawn points */
 	public List<Vec2f> playerSpawns = new ArrayList<>();
-	
+
 	/** World gravity vector */
 	public Vec2f gravity = new Vec2f(0);
-	
+
 	public Vec3f ambientLight = new Vec3f(.48, .48, .5);
 
 	public PhysicWorld physic;
-	
+
 	/** Map of all items available to item spawners */
 	public Map<String, Item> spawnList = new HashMap<>();
-	
+
 	/** Character spawns that have been used */
 	public ArrayList<Vec2f> usedSpawns = new ArrayList<>();
-	
+
 	/** All items currently on the map */
 	public ArrayList<Item> items = new ArrayList<>();
-	
+
 	/** Kill every Spatial that touches these bounds (min X, min Y, max X, max Y) */
 	public Vec4f killBound = new Vec4f(-100, -100, 100, 100);
-	
-	/** Base camera position, camera movement will be restricted into this field of view */
+
+	/**
+	 * Base camera position, camera movement will be restricted into this field of
+	 * view
+	 */
 	public Vec3f cameraBasePos = new Vec3f(0, 0, 8);
-	
+
 	public Set<Light> lights = new HashSet<>();
 
 	Timer spawnWeapon = new Timer(4);
 
 	public Arena() {
 		physic = new PhysicWorld(this);
+		Barrel bb = new Barrel(new Vec2f(0, 0));
+		bb.attachToParent(getArena(), "zasqdw");
 	}
 	
+
 	/**
 	 * Get this Arena
+	 * 
 	 * @return this
 	 */
 	@Override
@@ -72,33 +80,28 @@ public class Arena extends Entity {
 	 *         dernier utilise
 	 * @return Vec2f
 	 */
-	/*public Vec2f GetRandomRespawn() {
-		int randomNum = ThreadLocalRandom.current().nextInt(0, spawn.size());
-		while (dernierspawn == randomNum) {
-			randomNum = ThreadLocalRandom.current().nextInt(0, spawn.size());
-		}
-		dernierspawn = randomNum;
-		return spawn.get(randomNum);
-	}*/
+	/*
+	 * public Vec2f GetRandomRespawn() { int randomNum =
+	 * ThreadLocalRandom.current().nextInt(0, spawn.size()); while (dernierspawn ==
+	 * randomNum) { randomNum = ThreadLocalRandom.current().nextInt(0,
+	 * spawn.size()); } dernierspawn = randomNum; return spawn.get(randomNum); }
+	 */
 
 	/**
-	 * Get a spawn location that is not currently occupied by a Character
-	 * <br/> Use this to give a random spawn to each Character
+	 * Get a spawn location that is not currently occupied by a Character <br/>
+	 * Use this to give a random spawn to each Character
+	 * 
 	 * @author Shervin
 	 * @return Vec2f
 	 */
 	public Vec2f GetRandomRespawnch() {
-		/*int rand = ThreadLocalRandom.current().nextInt(spawn.size());
-		if (!spawnch.isEmpty()) {
-			while (spawnch.contains(spawn.get(rand))) {
-				rand = ThreadLocalRandom.current().nextInt(0, spawn.size());
-				spawn.get(rand);
-			}
-		}
-		if (!spawnch.contains(spawn.get(rand))) {
-			spawnch.add(spawn.get(rand));
-		}*/
-		Vec2f rand = new Vec2f(0,0);
+		/*
+		 * int rand = ThreadLocalRandom.current().nextInt(spawn.size()); if
+		 * (!spawnch.isEmpty()) { while (spawnch.contains(spawn.get(rand))) { rand =
+		 * ThreadLocalRandom.current().nextInt(0, spawn.size()); spawn.get(rand); } } if
+		 * (!spawnch.contains(spawn.get(rand))) { spawnch.add(spawn.get(rand)); }
+		 */
+		Vec2f rand = new Vec2f(0, 0);
 		return rand;
 	}
 
@@ -106,22 +109,24 @@ public class Arena extends Entity {
 		Vec2f randi = new Vec2f();
 
 		try {
-			int rand = ThreadLocalRandom.current().nextInt(0,playerSpawns.size());
-
+			int rand = (int) (Math.random()*playerSpawns.size());
+			
 			randi = playerSpawns.get(rand);
 			int max = 100;
 			int etapes = 0;
 			while (usedSpawns.contains(randi) && etapes < max) {
-				rand = ThreadLocalRandom.current().nextInt(playerSpawns.size());
+				rand =  (int) (Math.random()*playerSpawns.size());
 				randi = playerSpawns.get(rand);
 				etapes++;
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
-			Main.log.error("Failed to find a spawn point: "+e.getLocalizedMessage());
+			Main.log.error("Failed to find a spawn point: " + e.getLocalizedMessage());
 		}
 		usedSpawns.add(randi);
 		return randi;
 
 	}
+	
+	
 }
--- project/src/arenashooter/entities/spatials/Barrel.java
+++ project/src/arenashooter/entities/spatials/Barrel.java
@@ -16,14 +16,14 @@ public class Barrel extends RigidBodyContainer {
 	private float damage = 75;
 	private double health = 5;
 	private Mesh barrelMesh;
-	private Timer detonTimer = new Timer(0.25);
+	private Timer detonTimer = new Timer(4.25);
 	private Entity instigator;
 	private double timeSinceLastSpark = 0;
 
 	public Barrel(Vec2f position) {
 		super(new RigidBody(new ShapeBox(new Vec2f(0.75, 1)), position, 0, CollisionFlags.RIGIDBODY, 8, 1));
 
-		barrelMesh = new Mesh(new Vec3f(position.x, position.y, 0), "data/crate/barrel_grey.obj");
+		barrelMesh = new Mesh(new Vec3f(position.x, position.y, 0), "data/meshes/barrels/barrel_01.obj");
 		barrelMesh.attachToParent(this, "Barrel_Mesh");
 
 		detonTimer.attachToParent(this, "Detonation_Timer");
@@ -58,6 +58,8 @@ public class Barrel extends RigidBodyContainer {
 		Explosion explosion = new Explosion(getWorldPos(),
 				new DamageInfo(damage, DamageType.EXPLOSION, new Vec2f(), 5f, instigator), 10);
 		explosion.attachToParent(getArena(), explosion.genName());
+		Particles particles = new Particles(getWorldPos(), "data/particles/fire_sparkles.xml");
+		particles.attachToParent(getArena(), "fire_particles" + genName());
 		detach();
 	}
 
@@ -66,10 +68,10 @@ public class Barrel extends RigidBodyContainer {
 			detonate();
 		} else if(detonTimer.isProcessing()) {
 			timeSinceLastSpark += d;
-			for(int i = 0 ; i < timeSinceLastSpark/0.1 ; i++) {//new Particle every 0.1s
+			for(int i = 0 ; timeSinceLastSpark>=0.1 && i < timeSinceLastSpark/0.1 ; i++) {//new Particle every 0.1s
 				Particles particles = new Particles(getWorldPos(), "data/particles/fire_sparkles.xml");
 				particles.attachToParent(getArena(), "fire_particles" + genName());
-				Audio.playSound2D("data/sound/Tic.ogg", AudioChannel.SFX, 1, (float)(1 + detonTimer.getValueRatio()), localPosition);
+				Audio.playSound2D("data/sound/Tic.ogg", AudioChannel.SFX, 1, (float)(.1 + detonTimer.getValueRatio()), localPosition);
 			}
 			timeSinceLastSpark %= 0.1;
 		}
