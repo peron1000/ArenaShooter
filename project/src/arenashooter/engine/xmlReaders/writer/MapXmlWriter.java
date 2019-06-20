0c1518ae3a2018221c752b636482b9e931fa1cee
SnPop <shervinpedaran@yahoo.fr>
2019-06-20 17:52:13 +0200

    ...

--- project/src/arenashooter/engine/xmlReaders/writer/MapXmlWriter.java
+++ project/src/arenashooter/engine/xmlReaders/writer/MapXmlWriter.java
@@ -378,7 +378,7 @@ public class MapXmlWriter {
 			// Add current entity to parent
 			entitiesElem.appendChild(elem);
 
-			// Name//TODO
+			// Name
 			elem.setAttribute("name", entityName);
 
 			// Write children
