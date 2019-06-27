258f250222afd01c38c89c58f3651a2abe1ffe88
Sagany <nathanjojorio@gmail.com>
2019-06-27 18:10:56 +0200

    Can set an item proba while setting an Spawner

--- project/src/arenashooter/game/gameStates/editor/SpawnEditor.java
+++ project/src/arenashooter/game/gameStates/editor/SpawnEditor.java
@@ -1,10 +1,14 @@
 package arenashooter.game.gameStates.editor;
 
+import java.util.Map.Entry;
+
 import arenashooter.engine.math.Vec4f;
 import arenashooter.engine.ui.TabList;
 import arenashooter.engine.ui.Trigger;
+import arenashooter.engine.ui.UiElement;
 import arenashooter.engine.ui.UiGroup;
 import arenashooter.engine.ui.UiListVertical;
+import arenashooter.engine.ui.UiValuableButton;
 import arenashooter.engine.ui.simpleElement.Button;
 import arenashooter.engine.ui.simpleElement.Label;
 import arenashooter.engine.ui.simpleElement.UiImage;
@@ -15,9 +19,12 @@ import arenashooter.game.gameStates.editor.editorEnum.Ui_Input;
 public class SpawnEditor extends EntityEditor {
 
 	private TabList<Button> itemChooser = new TabList<>();
+	private Spawner spawner;
+	private UiListVertical<UiElement> itemsList = new UiListVertical<>();
 
 	public SpawnEditor(ArenaEditor mainMenu, Spawner spawner) {
 		super(mainMenu, spawner, "SPAWNER");
+		this.spawner = spawner;
 
 		if (mainMenu.arenaConstruction.items.isEmpty()) {
 			Label info = new Label("No items available");
@@ -43,52 +50,61 @@ public class SpawnEditor extends EntityEditor {
 				Button b = new Button(item.name);
 				b.setScale(buttonXScale, buttonYScale);
 				b.setOnArm(new Trigger() {
-					
+
 					@Override
 					public void make() {
 						ui_InputState = Ui_Input.DOUBLE;
-						
-						UiImage border = new UiImage(0, 0, 0, 1) , bg = new UiImage(.8, .8, .8, .5);
+
+						UiImage border = new UiImage(0, 0, 0, 1), bg = new UiImage(.8, .8, .8, .5);
 						border.setScale(41);
 						bg.setScale(39);
 						Label title = new Label("Enter item proba");
 						title.setScale(scaleText);
 						title.setPosition(0, -12);
-						doubleInputGroup = new UiGroup<>(border , bg , title);
-						
+						doubleInputGroup = new UiGroup<>(border, bg, title);
+
 						doubleInput.setPosition(0, 0);
 						doubleInput.reset();
 						doubleInput.setOnFinish(new Trigger() {
-							
+
 							@Override
 							public void make() {
 								ui_InputState = Ui_Input.NOTHING;
 								spawner.addItem(item.name, (int) doubleInput.getDouble());
 								current = menu;
+								updateListItem();
 							}
+
 						});
 						doubleInput.setOnCancel(new Trigger() {
-							
+
 							@Override
 							public void make() {
 								ui_InputState = Ui_Input.NOTHING;
+								updateListItem();
 							}
 						});
-						
+
 					}
 				});
 				listChoice.addElement(b);
 			}
-			
+
 			itemChooser.addBind("Choose an Item", listChoice);
 			itemChooser.setPosition(menu.getPosition());
 			itemChooser.setTitleScale(titleScale, titleScale);
 
 		}
+
+		menu.addBind("Items", itemsList);
+		menu.setArrowsDistance(15);
+		menu.setScaleArrows(8, 8);
 		
+		updateListItem();
+
 		initPosition();
 	}
-	
+
 	@Override
 	public void setPositionLerp(double x, double y, double lerp) {
 		double xDif = x - getPosition().x, yDif = y - getPosition().y;
@@ -101,6 +117,79 @@ public class SpawnEditor extends EntityEditor {
 		double xDif = x - getPosition().x, yDif = y - getPosition().y;
 		super.setPosition(x, y);
 		itemChooser.addToPosition(xDif, yDif);
+	}
+
+	private void updateListItem() {
+		itemsList.forEach(e -> itemsList.removeElement(e));
+		if (spawner.getAvailableItems().isEmpty()) {
+			Label label = new Label("Empty");
+			label.setScale(scaleText);
+			itemsList.addElement(label);
+		} else {
+			for (Entry<String, Integer> entry : spawner.getAvailableItems().entrySet()) {
+				UiValuableButton<Integer> button = new UiValuableButton<Integer>(entry.getKey(), entry.getValue());
+				button.setScale(buttonXScale, buttonYScale);
+				button.setRectangleVisible(true);
+				button.setOnArm(new Trigger() {
+					
+					@Override
+					public void make() {
+						ui_InputState = Ui_Input.DOUBLE;
+						doubleInputGroup = getGroupSettingProba();
+						doubleInput.reset();
+						doubleInput.setOnCancel(new Trigger() {
+							
+							@Override
+							public void make() {
+								ui_InputState = Ui_Input.NOTHING;
+							}
+						});
+						doubleInput.setOnFinish(new Trigger() {
+							
+							@Override
+							public void make() {
+								ui_InputState = Ui_Input.NOTHING;
+								Integer i = Integer.valueOf((int) doubleInput.getDouble());
+								if(i.equals(0)) {
+									spawner.getAvailableItems().remove(entry.getKey());
+								} else {
+									button.setValue(i);
+									spawner.getAvailableItems().put(entry.getKey(), i);
+								}
+								updateListItem();
+							}
+						});
+					}
+				});
+				itemsList.addElement(button);
+			}
+		}
+	}
+	
+	private UiGroup<UiElement> getGroupSettingProba(){
+		UiGroup<UiElement> ret = new UiGroup<>();
+		double textScale = 2.5;
+
+		// background
+		UiImage bg = new UiImage(.85, .8, .75, .7), border = new UiImage(0, 0, 0, 1);
+		bg.setScale(60, 35);
+		double toBorder = 1.5;
+		border.setScale(bg.getScale().x + toBorder, bg.getScale().y + toBorder);
+
+		// Title
+		Label titleLabel = new Label("Setting proba");
+		titleLabel.setScale(5.5);
+		titleLabel.setPosition(0, -12);
+		
+		Label instr = new Label("Enter 0 to remove this items");
+		instr.setScale(textScale);
+		instr.setPosition(0 , -8);
+		
+		doubleInput.setPosition(-1, 3);
+		
+		ret.addElements(border , bg , titleLabel , instr , doubleInput);
+		return ret;
+		
 	}
 
 }
