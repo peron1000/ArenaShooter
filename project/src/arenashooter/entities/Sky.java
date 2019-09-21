3758a6d5aba1f2421d8158666914604a33299c02
Kapouett <kapouett@orange.fr>
2019-09-21 15:27:52 +0200

    Fixed Sky not loading

--- project/src/arenashooter/entities/Sky.java
+++ project/src/arenashooter/entities/Sky.java
@@ -1,10 +1,14 @@
 package arenashooter.entities;
 
+import java.util.Set;
+
+import com.github.cliftonlabs.json_simple.JsonArray;
 import com.github.cliftonlabs.json_simple.JsonObject;
 
 import arenashooter.engine.Profiler;
 import arenashooter.engine.graphics.Material;
 import arenashooter.engine.graphics.Model;
+import arenashooter.engine.json.StrongJsonKey;
 import arenashooter.engine.math.Vec3f;
 
 /**
@@ -16,17 +20,15 @@ public class Sky extends Entity {
 	private static Model quad = null;
 	public Material material;
 
-	public Sky(Vec3f colorBot, Vec3f colorTop) {
+	private Sky() {
 		if(quad == null) quad = Model.loadQuad();
 		material = Material.loadMaterial("data/materials/sky.material");
-		setColors(colorBot, colorTop);
 		zIndex = -9999;
 	}
 	
-	public Sky(Material material) {
-		if(quad == null) quad = Model.loadQuad();
-		this.material = material;
-		zIndex = -9999;
+	public Sky(Vec3f colorBot, Vec3f colorTop) {
+		this();
+		setColors(colorBot, colorTop);
 	}
 	
 	public void setColors(Vec3f newColorBot, Vec3f newColorTop) {
@@ -67,7 +69,48 @@ public class Sky extends Entity {
 	 */
 	@Override
 	public Sky clone() {
-		return new Sky(material.clone());
+		Sky clone = new Sky();
+		clone.material = material.clone();
+		return clone;
+	}
+	
+	@Override
+	public Set<StrongJsonKey> getJsonKey() {
+		Set<StrongJsonKey> set = super.getJsonKey();
+		set.add(new StrongJsonKey() {
+			@Override
+			public Object getValue() {
+				return getColorTop();
+			}
+			@Override
+			public String getKey() {
+				return "colorTop";
+			}
+			@Override
+			public void useKey(JsonObject json) throws Exception {
+				JsonArray a = json.getCollection(this);
+				if (a != null)
+					setColorTop(Vec3f.jsonImport(a));
+			}
+		});
+		set.add(new StrongJsonKey() {
+			@Override
+			public Object getValue() {
+				return getColorBot();
+			}
+			@Override
+			public String getKey() {
+				return "colorBottom";
+			}
+			@Override
+			public void useKey(JsonObject json) throws Exception {
+				JsonArray a = json.getCollection(this);
+				if (a != null)
+					setColorBot(Vec3f.jsonImport(a));
+			}
+		});
+		
+		return set;
 	}
 	
 	@Override
@@ -75,6 +118,12 @@ public class Sky extends Entity {
 		JsonObject sky = super.getJson();
 		sky.putChain("colorBottom", getColorBot());
 		sky.putChain("colorTop", getColorTop());
+		return sky;
+	}
+
+	public static Sky fromJson(JsonObject json) throws Exception {
+		Sky sky = new Sky();
+		useKeys(sky, json);
 		return sky;
 	}
 }
