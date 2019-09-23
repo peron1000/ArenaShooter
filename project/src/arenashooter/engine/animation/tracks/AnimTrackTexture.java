314f3c013d268d3b344a0426381cfab94e4ee923
Kapouett <kapouett@orange.fr>
2019-09-23 09:35:26 +0200

    Replaced some GL specific classes with interfaces

--- project/src/arenashooter/engine/animation/tracks/AnimTrackTexture.java
+++ project/src/arenashooter/engine/animation/tracks/AnimTrackTexture.java
@@ -3,8 +3,8 @@ package arenashooter.engine.animation.tracks;
 import java.util.HashMap;
 import java.util.Map;
 
-import arenashooter.engine.graphics.GLTexture;
 import arenashooter.engine.graphics.Texture;
+import arenashooter.game.Main;
 
 /**
  * Texture animation track without interpolation
@@ -16,15 +16,15 @@ public class AnimTrackTexture extends AnimTrack<Texture> {
 	}
 
 	@Override
-	public GLTexture valueAt(double time) {
-		return (GLTexture)values[prevKeyframe(time)];
+	public Texture valueAt(double time) {
+		return (Texture)values[prevKeyframe(time)];
 	}
 
 	@Override
 	public Map<Double, Texture> extractData() {
 		Map<Double, Texture> keyframes = new HashMap<>();
 		for(int i=0; i<times.length; i++)
-			keyframes.put(times[i], GLTexture.loadTexture( ((GLTexture)values[i]).getPath() ));
+			keyframes.put(times[i], Main.getRenderer().loadTexture( ((Texture)values[i]).getPath() ));
 		return keyframes;
 	}
 	
